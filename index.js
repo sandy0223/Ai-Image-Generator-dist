1438ad295ab938fbf9bf8ca1756dcb426a131c6f
Sandeep Behera <123327853+sandy0223@users.noreply.github.com>
2024-12-28 19:39:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+
+
+app.listen(3001, () => {
+  console.log('Server is running on http://localhost:3001');
+});
